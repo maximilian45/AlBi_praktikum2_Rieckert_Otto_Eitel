a7308a6f93bb4f3b326734046d11624b7dae5057
acedon <lackyluck@hotmail.de>
2018-01-09 17:55:17 +0000

    Upload New File

---
+++ Knoten.java
@@ -0,0 +1,33 @@
+public class Knoten extends GenomAssembling{
+	protected int id;
+	protected int[] inKantenIds;
+	protected int[] outKantenIds;
+	protected String value;
+	public Knoten(int id, int[] inKantenIds, int[] outKantenIds, String value){
+		this.id = id;
+		this.inKantenIds = inKantenIds;
+		this.outKantenIds = outKantenIds; 
+		this.value = value;
+	}
+	public int getId(){
+		return this.id;
+	}
+	public int[] getInKantenIds(){
+		return this.inKantenIds;
+	}
+	public void setInKantenIds(int[] newIds){
+		this.inKantenIds = newIds;
+	}
+	public int[] getOutKantenIds(){
+		return this.outKantenIds;
+	}	
+	public void setOutKantenIds(int[] newIds){
+		this.outKantenIds = newIds;
+	}
+	public String getValue(){
+		return this.value;
+	}
+	public void setValue(String value){
+		this.value = value;
+	}
+}
