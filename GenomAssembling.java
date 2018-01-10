88d688e5edacac7cf041e164853ac2880d2c7c27
acedon <lackyluck@hotmail.de>
2018-01-10 18:23:48 +0000

    Replace GenomAssembling.java neue version, baut kanten und knoten anscheinend richtig auf, allerdings speichere ich sie zwischen durch als ArrayList von Objects  ab und kann dadurch hinterher nicht mehr auf die attribute der Kanten und Knoten zugrefen, da sie sich auch nicht von Object zurück in Kanten/Knoten umwandeln lassen. Allerdings bekomme ich es gerade nicht hin Listen von Knoten bzw Kanten zu erstellen. Wenn das klappt müsste es eigentlich alle möglichen knoten und kanten berechnen. Diese müssten nur noch verschmolzen werden um den Graph fertig zu stellen: Ein weiteres Problem ist, dass er mit dem großen BEispiel datensatz aus dem Speicher rausläuft, also wird diese lösung wahrscheinlich nicht funktional sein.

--- GenomAssembling.java
+++ GenomAssembling.java
@@ -9,7 +9,7 @@ class GenomAssembling{
 		
 		List<String> reads = new ArrayList<String>();
 		
-		for(int i = 0; i <= 33609; i++){
+		for(int i = 0; i </*=*/ 1 /*33609*/; i++){
 			reads.add(br.readLine());
 			
 		}
@@ -37,5 +37,83 @@ class GenomAssembling{
 			}
 			brokenReads.add(temp);
 		}	
+	
+	
+	int countEdge = 0;
+	int countNode= 0;
+	Object[] dbgraph = new Object[1];
+	//Kanten[] allEdges = new Kanten[1];
+	//Knoten[] allNodes = new Knoten[2];
+	
+	ArrayList<Object> allEdges = new ArrayList<Object>();
+	ArrayList<Object> allNodes = new ArrayList<Object>();
+	
+	for(int i = 0; i < brokenReads.size(); i++){
+		ArrayList<String> temp = new ArrayList<String>();
+		temp = brokenReads.get(i);
+		for(int j = 0; j < temp.size();j++){
+			Kante edge = new Kante(countEdge,temp.get(j));
+			countEdge += 1;
+			Knoten inNode = new Knoten(countNode, temp.get(j).substring(0,(temp.size()-2)));
+			countNode += 1;
+			Knoten outNode = new Knoten(countNode,temp.get(j).substring(1));
+			countNode += 1;
+			
+			int[] temp2 = new int[1];
+			temp2[0] = 0;
+			int[] temp3 = new int[1];
+			temp3[0] = edge.id;
+			edge.startKnotenId = inNode.id;
+			edge.endKnotenId = outNode.id;
+			inNode.inKantenIds = temp2;
+			inNode.outKantenIds = temp3;
+			outNode.inKantenIds = temp3;
+			outNode.outKantenIds = temp2;
+			
+			
+			//if(j==0){
+				
+				
+				allEdges.add(edge);
+				allNodes.add(inNode);
+				allNodes.add(outNode);
+				
+				
+			/*}else{
+				//Kanten[] newEdges = new Kanten[(allEdges.length())+1];
+				//for(int k = 0; k < allEdges.length(); k++){
+				//	newEdges.add(allEdges[k]);
+				//}
+				//newEdges.add(edge);
+				//allEdges = newEdges;
+				
+				
+				Knoten[] newNodes = new Knoten[(allNodes.length())+2];
+				for(int l = 0; l < allNodes.length();l++){
+					newNodes.add(allNodes[l]);
+				}
+				newNodes.add(inNode);
+				newNodes.add(outNode);
+				allNodes = newNodes;
+			}*/
+			
+			
+		}
+	}
+	ArrayList<String> temp = new ArrayList<String>();
+	temp = brokenReads.get(0);
+	String temp2 = temp.get(0);
+	Object tempKante = allEdges.get(0);
+	Object tempInNode = allNodes.get(0);
+	Object tempOutNode = allNodes.get(1);
+	System.out.println("READ !!!!!");
+	System.out.println(temp2);
+	System.out.println("ALL EDGES !!!!!");
+	System.out.println(tempKante);
+	System.out.println("IN NODE !!!!!");
+	System.out.println(tempInNode);
+	System.out.println("OUT NODE !!!!!");
+	System.out.println(tempOutNode);
+	
 	}
 }
