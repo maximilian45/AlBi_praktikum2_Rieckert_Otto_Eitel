b7f734e469c7f88d2a9cd53f5952caeef2bd33c3
acedon <lackyluck@hotmail.de>
2018-01-11 20:30:00 +0000

    Update DBGraph.java

--- DBGraph.java
+++ DBGraph.java
@@ -44,7 +44,7 @@ public final class DBGraph {
 		for(int l = 0; l < reads.size(); l++){
 			ArrayList<String> temp = new ArrayList<String>();
 			String read = reads.get(l);
-			//durchgehen für die ersten stellen des einzelnen reads 
+			//durchgehen fr die ersten stellen des einzelnen reads 
 			for(int i = 0; i < reads.get(l).length()-k ; i++){
 				String pattern = "";   //read.substring(i,(k-1+i));
 				for(int j = 0; j<k ; j++){
@@ -126,7 +126,7 @@ public final class DBGraph {
 			}
 			
 		}*/
-		//Aufbau von Kanten zu jedem read mit den dazugehörigen Knoten
+		//Aufbau von Kanten zu jedem read mit den dazugehrigen Knoten
 		for(int i = 0; i<reads.size();i++){
 			ArrayList<String> temp = reads.get(i);
 			for(int j = 0; j< temp.size(); j++){
@@ -138,8 +138,8 @@ public final class DBGraph {
 				g.addEdge(prefix,suffix, new RelationshipEdge(prefix,suffix,read));
 			}
 		}
-		/*
-		// Verschmelzen von gleichen Knoten
+		
+		// Verschmelzen von gleichen Knoten (NOCH NICHT FERTIG!!!)
 		for(RelationshipEdge edge : g.edgeSet()){
 			String target = edge.getV2().toString();
 			for(RelationshipEdge edge2 :g.edgeSet()){
@@ -153,9 +153,8 @@ public final class DBGraph {
 		//Test Ausgabe
 		for(RelationshipEdge edge : g.edgeSet()){
 		System.out.println("KANTEN LABEL!!!" + edge.toString());
-		}*/
+		}
 		
-		System.out.println(g.toString());
 		
 		
 		
