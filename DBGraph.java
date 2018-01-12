481e6ddab3ccfc3a66c4e50e7b4583913d2d5480
acedon <lackyluck@hotmail.de>
2018-01-12 01:06:37 +0000

    Replace DBGraph.java

--- DBGraph.java
+++ DBGraph.java
@@ -19,45 +19,6 @@ public final class DBGraph {
 		
 	}
 	
-	private static ArrayList<ArrayList<String>> generateReads() throws IOException{
-	
-		FileReader fr = new FileReader("C:\\Users\\Acedon\\Desktop\\Uni\\albi2\\HelloJGraphT\\src\\org\\jgrapht\\demo\\test.txt");
-
-		BufferedReader br = new BufferedReader(fr);
-		
-		List<String> reads = new ArrayList<String>();
-		
-		for(int i = 1; i <=33609; i++){
-			reads.add(br.readLine());
-			
-		}
-		br.close();
-		
-		int k = 85;
-		ArrayList<ArrayList<String>> brokenReads = new ArrayList<ArrayList<String>>();
-		
-		//Bilden der reads breaks
-		
-		// EVT INERFFIZIENT
-		
-		//nehmen des ersten reads
-		for(int l = 0; l < reads.size(); l++){
-			ArrayList<String> temp = new ArrayList<String>();
-			String read = reads.get(l);
-			//durchgehen für die ersten stellen des einzelnen reads 
-			for(int i = 0; i < reads.get(l).length()-k ; i++){
-				String pattern = "";   //read.substring(i,(k-1+i));
-				for(int j = 0; j<k ; j++){
-					pattern += read.charAt(i+j);
-				}
-				temp.add(pattern);
-			}
-			brokenReads.add(temp);
-		}
-		return brokenReads;
-		
-	}
-	
 	public static String buildPrefix(String read){
 		String prefix = "";
 		for(int i = 0;i<=read.length()-2;i++){
@@ -96,6 +57,55 @@ public final class DBGraph {
 		return true;
 	}
 	
+	private static ArrayList<ArrayList<String>> generateReads() throws IOException{
+	
+		FileReader fr = new FileReader("C:\\Users\\Acedon\\Desktop\\Uni\\albi2\\HelloJGraphT\\src\\org\\jgrapht\\demo\\test2.txt");
+
+		BufferedReader br = new BufferedReader(fr);
+		
+		ArrayList<String> reads = new ArrayList<String>();
+		
+		for(int i = 1; i <=33609; i++){
+			reads.add(br.readLine());
+			
+		}
+		br.close();
+
+		int k = 85;
+		ArrayList<ArrayList<String>> output = new ArrayList<ArrayList<String>>();
+		ArrayList<String> brokenReads = new ArrayList<String>();
+		ArrayList<String> knotenListe = new ArrayList<String>();
+		Set<String> hs = new HashSet<>();
+		
+		//Bilden der reads breaks
+		
+		// EVT INERFFIZIENT
+		
+		//nehmen des ersten reads
+		for(int l = 0; l < reads.size(); l++){
+			//ArrayList<String> temp = new ArrayList<String>();
+			String read = reads.get(l);
+			//durchgehen für die ersten stellen des einzelnen reads 
+			for(int i = 0; i < reads.get(l).length()-k ; i++){
+				String pattern = "";   //read.substring(i,(k-1+i));
+				for(int j = 0; j<k ; j++){
+					pattern += read.charAt(i+j);
+				}
+				brokenReads.add(pattern);
+				hs.add(buildPrefix(pattern));
+				hs.add(buildSuffix(pattern));
+				
+			}
+		}
+		knotenListe.addAll(hs);
+		output.add(brokenReads);
+		output.add(knotenListe);
+		return output;
+		
+	}
+	
+
+	
 	//FremdCode
 	public static class RelationshipEdge<V> extends DefaultEdge {
         private V v1;
@@ -129,94 +139,65 @@ public final class DBGraph {
                 new DirectedMultigraph<String, RelationshipEdge>(
                         new ClassBasedEdgeFactory<String, RelationshipEdge>(RelationshipEdge.class));
 		//FC Ende
-		
-        //Test Knoten
-		/*g.addVertex("v1");
-		g.addVertex("v2");
-		g.addEdge("v1", "v2", new RelationshipEdge("v1","v2","e1"));
-		*/
-
-		
-		try{
-		ArrayList<ArrayList<String>> reads = generateReads();
-
-		/* Schleife zum Testen ob reads richtig generiert wurden
-		for(int i = 0; i< reads.size();i++){
-			ArrayList<String> temp = reads.get(i);
-			for(int j = 0; j<temp.size();j++){
-				System.out.println(temp.get(j));
-			}
 			
-		}
-		//Aufbau von Kanten zu jedem read mit den dazugehörigen Knoten
-		for(int i = 0; i<reads.size();i++){
-			ArrayList<String> temp = reads.get(i);
-			for(int j = 0; j< temp.size(); j++){
-				String read = temp.get(j);
-				String prefix = buildPrefix(read);
-				String suffix = buildSuffix(read);
-				g.addVertex(prefix);
-				g.addVertex(suffix);
-				g.addEdge(prefix,suffix, new RelationshipEdge(prefix,suffix,read));
-			}
-		}
-		
-		// Verschmelzen von gleichen Knoten
-		for(RelationshipEdge edge : g.edgeSet()){
-			String target = edge.getV2().toString();
-			for(RelationshipEdge edge2 :g.edgeSet()){
-				String source = edge2.getV1().toString();
-				
-			}
-		}*/
+		try{
+			ArrayList<ArrayList<String>> arL = generateReads();
+			ArrayList<String> reads = arL.get(0);
+			ArrayList<String> knotenListe = arL.get(1);
+
+			
+		/*   HABE ICH ALLES IN DIE FUNKTION generateReads ausgelagert um die beiden schleifen zu sparen!!!!!!!!!!!!!!
 		//Aufbau der Liste der k-1 mere 
 		ArrayList<String> knotenListe = new ArrayList<String>();
 		for(int i = 0; i<reads.size();i++){
-			ArrayList<String> temp = reads.get(i);
-			for(int j = 0; j< temp.size(); j++){
-				knotenListe.add(buildPrefix(temp.get(j)));
-				knotenListe.add(buildSuffix(temp.get(j)));
+			String temp = reads.get(i);
+			for(int j = 0; j< temp.length(); j++){
+				knotenListe.add(buildPrefix(temp));
+				knotenListe.add(buildSuffix(temp));
 			}
 		}
+		
+		
 		// Löschen von dublikaten durch abspeichern in ein set, dass keine dublikate erlaubt 
 		// und dann rückführen ind die knotenListe   knotenListe enthält alle k-1 mere der Reads, also alle suffixe und prefixe
 		Set<String> hs = new HashSet<>();
 		hs.addAll(knotenListe);
 		knotenListe.clear();
 		knotenListe.addAll(hs);
+		*/
+		System.out.println("TEST!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+
 		
-		//Liste der kmere von einer ArrayList ausArrayLists in eine einfach ArrayList umgewandelt splicedreads beinhaltet alle 85mere der reads
-		ArrayList<String> splicedReads = new ArrayList<String>();
-		for(int i = 0; i<reads.size();i++){
-			ArrayList<String> temp = reads.get(i);
-			for(int j = 0; j< temp.size(); j++){
-				splicedReads.add(temp.get(j));
-			}
-		}
-		
-		//Aufbau des Graphen
+		//Aufbau des Graphen       Im Momentanen Schleifen Format sind das leider über 2069311321980 iterationen
 		for(int i=0;i<knotenListe.size();i++){
 			String temp1 = knotenListe.get(i);
 			knotenListe.remove(i);
 			for(int j = 0; j<knotenListe.size();j++){
 				String temp2 = knotenListe.get(j);
-				for(int k =0; k< splicedReads.size();k++){
-					if((isPrefix(temp1,splicedReads.get(k))) && ( isSuffix(temp2,splicedReads.get(k) ) ) ){
-						g.addEdge(temp1,temp2, new RelationshipEdge(temp1,temp2,(splicedReads.get(k))));
+				for(int k =0; k< reads.size();k++){
+					if((isPrefix(temp1,reads.get(k))) && ( isSuffix(temp2,reads.get(k) ) ) ){
+						g.addVertex(temp1);
+						g.addVertex(temp2);
+						g.addEdge(temp1,temp2, new RelationshipEdge(temp1,temp2,(reads.get(k))));
 					}else{
-						if((isPrefix(temp2,splicedReads.get(k))) && ( isSuffix(temp1,splicedReads.get(k) ) )){
-							g.addEdge(temp2,temp1, new RelationshipEdge(temp2,temp1,(splicedReads.get(k))));
+						if((isPrefix(temp2,reads.get(k))) && ( isSuffix(temp1,reads.get(k) ) )){
+							g.addVertex(temp1);
+							g.addVertex(temp2);
+							g.addEdge(temp2,temp1, new RelationshipEdge(temp2,temp1,(reads.get(k))));
 						}
 					}
 				}
 			}
 		}
-		
+		/*
 		//Test Ausgabe
 		for(RelationshipEdge edge : g.edgeSet()){
-		System.out.println("KANTEN LABEL!!!" + edge.toString());
+		System.out.println("SOURCE!!!!" + edge.getV1().toString());
+		System.out.println(edge.toString());
+		System.out.println("Tagret!!!!" + edge.getV2().toString());
+		break;
 		}
-		
+		*/
 		
 		
 		
